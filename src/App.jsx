b3e6593392ff4876d64e6d8ce03f23ac9c17eb1a
luckybingi <luckybingi2005@gmail.com>
2025-07-25 10:39:26 +0530

    testing 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
-// import "./index.css";
-// import { Toaster } from 'react-hot-toast';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import LoginPage from "./pages/LoginPage";
-// import SignupPage from "./pages/SignupPage";
-// import ChatPage from "./pages/ChatPage";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Toaster position="top-center" reverseOrder={false} />
-//       <Routes>
-//         <Route path="/" element={<LoginPage />} />
-//           <Route path="/login" element={<LoginPage />} />   {/* ✅ Add this */}
-//         <Route path="/signup" element={<SignupPage />} />
-//         <Route path="/chats" element={<ChatPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import ChatPage from "./pages/ChatPage";
+import { useEffect, useState } from "react";
 
 function App() {
-  const user = JSON.parse(localStorage.getItem("userInfo"));
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(storedUser);
+  }, []);
 
   return (
     <BrowserRouter>
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        {/* Redirect root to login or chats based on auth */}
         <Route path="/" element={user ? <Navigate to="/chats" /> : <Navigate to="/login" />} />
         <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/chats" />} />
         <Route path="/signup" element={!user ? <SignupPage /> : <Navigate to="/chats" />} />
